8dedee5ea442299f5ceb88d41eecf4ce0fc61b80
Sandro <31536101+sandromund@users.noreply.github.com>
2023-02-23 20:39:42 +0100

    Update tasks.py

--- src/tasks.py
+++ src/tasks.py
@@ -1,5 +1,6 @@
 import pywhatkit as kit
 import requests
+from decouple import config
 
 
 def search_on_youtube(query):
@@ -12,3 +13,12 @@ def search_on_google(query):
 
 def get_random_joke():
     return requests.get("https://icanhazdadjoke.com/", headers={'Accept': 'application/json'}).json()["joke"]
+
+
+def get_weather_report(c):
+    id = config("OPENWEATHER_APP_ID")
+    res = res = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={id}")
+    weather = res["weather"][0]["main"]
+    temperature = res["main"]["temp"]
+    feels_like = res["main"]["feels_like"]
+    return weather, f"{temperature}℃", f"{feels_like}℃"
