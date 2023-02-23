9476bd151b3546842b1b742a8030c162a081ac8b
Sandro <31536101+sandromund@users.noreply.github.com>
2023-02-23 20:39:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from decouple import config
 from src.assistant import Assistant
 
 if __name__ == '__main__':
-    ai = Assistant(user=config('USER'), name=config('BOTNAME'))
+    ai = Assistant(user=config('USER'),
+                   name=config('BOTNAME'))
     ai.greeting()
     while True:
         query = ai.listen()
